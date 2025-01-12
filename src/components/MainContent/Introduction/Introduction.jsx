fe73df135dfed57b76494fc3a6b70889e28b75a7
cendyz <arek57823@gmail.com>
2025-01-12 10:47:58 +0100

    introduction desktop ok

--- src/components/MainContent/Introduction/Introduction.jsx
+++ src/components/MainContent/Introduction/Introduction.jsx
@@ -1,21 +1,27 @@
 import img from '../../../images/mobile/image-interactive.jpg'
+import desktopImg from '../../../images/desktop/image-interactive.jpg'
 import styles from './Introduction.module.scss'
 
 const Introduction = () => {
 	return (
 		<section className={styles.section}>
-			<img
-				src={img}
-				alt='Guy playing with google vr'
-				className={styles.img}
-			/>
-			<h2 className={styles.title}>The leader in interactive VR</h2>
-			<p className={styles.desc}>
-				Founded in 2011, Loopstudios has been producing world-class virtual
-				reality projects for some of the best companies around the globe.
-				Our award-winning creations have transformed businesses through
-				digital experiences that bind to their brand.
-			</p>
+			<picture>
+				<source srcSet={desktopImg} media='(min-width:992px)' />
+				<img
+					src={img}
+					alt='Guy playing with google vr'
+					className={styles.img}
+				/>
+			</picture>
+			<div className={styles.textBox}>
+				<h2 className={styles.title}>The leader in interactive VR</h2>
+				<p className={styles.desc}>
+					Founded in 2011, Loopstudios has been producing world-class
+					virtual reality projects for some of the best companies around
+					the globe. Our award-winning creations have transformed
+					businesses through digital experiences that bind to their brand.
+				</p>
+			</div>
 		</section>
 	)
 }
