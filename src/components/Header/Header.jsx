bca51a67ec5662b91bb730634732d7828f9264d1
cendyz <arek57823@gmail.com>
2025-01-12 10:19:23 +0100

    nav header desktop ok

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import styles from './Header.module.scss'
 import headerPattern from '../../images/mobile/image-hero.jpg'
+import desktopPattern from '../../images/desktop/image-hero.jpg'
 
 const Header = () => {
 	return (
 		<header className={styles.header}>
-			<img
-				src={headerPattern}
-				alt='Guy playing with vr googles on'
-				className={styles.pattern}
-			/>
+			<picture>
+				<source srcSet={desktopPattern} media='(min-width: 992px)' />
+				<img
+					src={headerPattern}
+					alt='Guy playing with vr googles on'
+					className={styles.pattern}
+				/>
+			</picture>
 			<div className={styles.textBox}>
 				<h1 className={styles.title}>
 					Immersive experiences that deliver
--- src/components/Nav/NavDesktop/NavDesktop.jsx
+++ src/components/Nav/NavDesktop/NavDesktop.jsx
@@ -1,8 +1,21 @@
 import styles from './NavDesktop.module.scss'
+import logo from '../../../images/logo.svg'
+import { linkData } from '../../../data'
 
 const NavDesktop = () => {
-  return (
-    <nav className={styles.nav}></nav>
-  )
+	return (
+		<nav className={styles.nav}>
+			<img src={logo} alt='Logo loopstudios' className={styles.logo} />
+			<div className={styles.linksBox}>
+				{linkData.map(({ link }, index) => {
+					return (
+						<a href='#' className={styles.link} key={index}>
+							{link}
+						</a>
+					)
+				})}
+			</div>
+		</nav>
+	)
 }
 export default NavDesktop
