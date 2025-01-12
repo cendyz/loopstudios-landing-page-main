ef59a12aa51a2c01bc81fba50249761be8a9c0ac
cendyz <arek57823@gmail.com>
2025-01-12 11:15:33 +0100

    creations desktop ok

--- src/components/MainContent/Creations/Creations.jsx
+++ src/components/MainContent/Creations/Creations.jsx
@@ -3,20 +3,26 @@ import { creationsData } from '../../../data'
 
 const Creations = () => {
 	return (
-		<section>
-			<h2 className={styles.title}>our creations</h2>
+		<section className={styles.section}>
+			<div className={styles.upBox}>
+				<h2 className={styles.title}>our creations</h2>
+				<button className={styles.desktopBtn}>see all</button>
+			</div>
 			<div className={styles.imagesContent}>
-				{creationsData.map(({ title, img, alt }, index) => {
+				{creationsData.map(({ title, img, img2, alt }, index) => {
 					return (
 						<div className={styles.imageBox} key={index}>
-							<img src={img} alt={alt} className={styles.img} />
+							<picture>
+								<source srcSet={img2} media='(min-width:992px)' />
+								<img src={img} alt={alt} className={styles.img} />
+							</picture>
 							<p className={styles.secondTitle}>{title}</p>
 						</div>
 					)
 				})}
-			<button className={styles.btn} type='button'>
-				see all
-			</button>
+				<button className={styles.btn} type='button'>
+					see all
+				</button>
 			</div>
 		</section>
 	)
--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 import openHamburger from './images/icon-hamburger.svg'
 import closeamburger from './images/icon-close.svg'
+
 import earthImg from './images/mobile/image-deep-earth.jpg'
 import arcadeImg from './images/mobile/image-night-arcade.jpg'
 import soccerImg from './images/mobile/image-soccer-team.jpg'
@@ -12,6 +13,15 @@ import facebookIcon from './images/icon-facebook.svg'
 import twitterIcon from './images/icon-twitter.svg'
 import pinterestIcon from './images/icon-pinterest.svg'
 import instagramIcon from './images/icon-instagram.svg'
+
+import earthImg2 from './images/desktop/image-deep-earth.jpg'
+import arcadeImg2 from './images/desktop/image-night-arcade.jpg'
+import soccerImg2 from './images/desktop/image-soccer-team.jpg'
+import gridImg2 from './images/desktop/image-grid.jpg'
+import aboveImg2 from './images/desktop/image-from-above.jpg'
+import pocketImg2 from './images/desktop/image-pocket-borealis.jpg'
+import curiosityImg2 from './images/desktop/image-curiosity.jpg'
+import fisheyeImg2 from './images/desktop/image-fisheye.jpg'
 
 export const linkData = [
 	{
@@ -46,41 +56,49 @@ export const creationsData = [
 	{
 		title: 'deep earth',
 		img: earthImg,
+		img2: earthImg2,
 		alt: 'Earth view from cosmos',
 	},
 	{
 		title: 'night arcade',
 		img: arcadeImg,
+		img2: arcadeImg2,
 		alt: 'Arcade console',
 	},
 	{
 		title: 'soccer team vr',
 		img: soccerImg,
+		img2: soccerImg2,
 		alt: 'Guy running while playing soccer',
 	},
 	{
 		title: 'the grid',
 		img: gridImg,
+		img2: gridImg2,
 		alt: 'Car with mobil and michelin logo',
 	},
 	{
 		title: 'from up above vr',
 		img: aboveImg,
+		img2: aboveImg2,
 		alt: 'Road with sharp curves',
 	},
 	{
 		title: 'pocket borealis',
 		img: pocketImg,
+		img2: pocketImg2,
 		alt: 'aurora borealis',
 	},
 	{
 		title: 'the curiosity',
 		img: curiosityImg,
+		img2: curiosityImg2,
 		alt: 'broadcasting station on mars',
 	},
 	{
 		title: 'make it fisheye',
 		img: fisheyeImg,
+		img2: fisheyeImg2,
 		alt: 'fisheye',
 	},
 ]
